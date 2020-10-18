91453cf4f491dfffe952b5f28fc7ba5f56f34d67
Arodriguez1030 <arodriguez1030@gmail.com>
2020-10-17 22:00:18 -0400

    completed signup form and page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,14 @@
 class UsersController < ApplicationController
     get '/signup' do
-    
         erb :"/user/new"
+    end
+
+    post '/signup' do
+        @user = User.create(params["user"])
+        redirect to "/login"
+    end
+
+    get '/login' do
+        "login"
     end
 end
