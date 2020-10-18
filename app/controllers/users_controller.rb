fdd7d5ed54b54573f11f00789c72dc3167864ed0
Arodriguez1030 <arodriguez1030@gmail.com>
2020-10-18 10:18:10 -0400

    created login page and authentication

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,12 +3,27 @@ class UsersController < ApplicationController
         erb :"/user/new"
     end
 
+    get '/login' do
+        erb :"/user/login"
+    end
+
     post '/signup' do
         @user = User.create(params["user"])
         redirect to "/login"
     end
 
-    get '/login' do
-        "login"
+    post '/login' do
+        @user= User.find_by(email: params["email"])
+        if @user && @user.authenticate(params["password"])
+            session[:user_id] = @user.id
+            redirect to "/users/#{@user.id}"
+        else
+            redirect  to "/login"
+        end
+    end
+
+    get '/users/:id' do
+        @user = find_by_id(params[:id])
+        
     end
 end
