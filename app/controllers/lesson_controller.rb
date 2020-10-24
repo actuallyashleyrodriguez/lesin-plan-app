560e9694eca0c5e32d9532ff49ca9a9c1275892b
Arodriguez1030 <arodriguez1030@gmail.com>
2020-10-23 22:29:38 -0400

    added page to show singular lesson

--- app/controllers/lesson_controller.rb
+++ app/controllers/lesson_controller.rb
@@ -9,8 +9,12 @@ class LessonController < ApplicationController
 
         erb  :"/lesson/index"
     end
-    
+
     get  '/lessons/:id' do
+        @lesson = Lesson.find_by_id(params[:id])
+        @user = @lesson.user
+
+        erb :"/lesson/show"
     end
 
     #update
